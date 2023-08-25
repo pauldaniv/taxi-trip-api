59df9685f676938d95a93217af2d460b3d149725
Pavlo <paulsegfault@gmail.com>
2023-08-25 22:23:43 +0300

    Add trusted packages to kafka

--- service/src/main/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListener.java
+++ service/src/main/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListener.java
@@ -9,8 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
