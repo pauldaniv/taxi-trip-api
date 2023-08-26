e5cf46da555e42fbbc684c50c918d22fed17c95c
Pavlo <paulsegfault@gmail.com>
2023-08-26 12:01:07 +0300

    Fix tests

--- service/src/test/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListenerTest.java
+++ service/src/test/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListenerTest.java
@@ -1,8 +1,6 @@
 package com.pauldaniv.promotion.yellowtaxi.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pauldaniv.promotion.yellowtaxi.model.TaxiTrip;
 import com.pauldaniv.promotion.yellowtaxi.persistence.TaxiTripDAO;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +11,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -24,48 +20,36 @@ public class TaxiTripListenerTest {
 
     @Mock
     private TaxiTripDAO taxiTripDAO;
-    @Mock
-    private ObjectMapper objectMapper;
 
     private TaxiTripListener taxiTripListener;
 
     @BeforeTest
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        taxiTripListener = new TaxiTripListener(taxiTripDAO, objectMapper);
+        taxiTripListener = new TaxiTripListener(taxiTripDAO);
     }
 
     @Test
     public void consumesTaxiTripSuccessfully() throws JsonProcessingException {
 
-        final String tripData = "aoeu";
         final TaxiTrip taxiTrip = TaxiTrip.builder().build();
-
-        when(objectMapper.readValue(tripData, TaxiTrip.class))
-                .thenReturn(taxiTrip);
 
         when(taxiTripDAO.store(taxiTrip))
                 .thenReturn(taxiTrip);
 
-        taxiTripListener.consumeTrip(tripData);
-        verify(objectMapper, only()).readValue(tripData, TaxiTrip.class);
+        taxiTripListener.consumeTrip(taxiTrip);
         verify(taxiTripDAO, only()).store(taxiTrip);
     }
 
     @Test
     public void consumesTaxiTripExceptionally() throws JsonProcessingException {
+        final TaxiTrip taxiTrip = TaxiTrip.builder().build();
 
-        final String tripData = "aoeu";
-
-        when(objectMapper.readValue(tripData, TaxiTrip.class))
-                .thenThrow(new JsonMappingException(null, "test exception"));
-
-        taxiTripListener.consumeTrip(tripData);
-        verify(objectMapper, only()).readValue(tripData, TaxiTrip.class);
+        taxiTripListener.consumeTrip(taxiTrip);
     }
 
     @AfterMethod
     public void teardown() {
-        Mockito.reset(taxiTripDAO, objectMapper);
+        Mockito.reset(taxiTripDAO);
     }
 }
