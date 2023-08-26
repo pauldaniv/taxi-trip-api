0c5858155c09991111fa35f95ead2dd16178857c
Pavlo <paulsegfault@gmail.com>
2023-08-26 11:40:36 +0300

    Fix trip consumer

--- service/src/main/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListener.java
+++ service/src/main/java/com/pauldaniv/promotion/yellowtaxi/service/TaxiTripListener.java
@@ -1,7 +1,5 @@
 package com.pauldaniv.promotion.yellowtaxi.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pauldaniv.promotion.yellowtaxi.model.TaxiTrip;
 import com.pauldaniv.promotion.yellowtaxi.persistence.TaxiTripDAO;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +13,14 @@ import org.springframework.stereotype.Service;
 public class TaxiTripListener {
 
     private final TaxiTripDAO taxiTripDAO;
-    private final ObjectMapper objectMapper;
 
     @KafkaListener(id = "main", topics = {"taxi-trips"})
-    public void consumeTrip(final String tripRequest) {
+    public void consumeTrip(final TaxiTrip taxiTrip) {
         try {
-            final TaxiTrip taxiTrip = objectMapper.readValue(tripRequest, TaxiTrip.class);
             taxiTripDAO.store(taxiTrip);
             log.info("msg=received_trip trip={}", taxiTrip);
-        } catch (JsonProcessingException e) {
-            log.error("Failed to parse object. Raw data: {}. Reason: {}", tripRequest, e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("Failed to parse object. Raw data: {}. Reason: {}", taxiTrip, e.getMessage(), e);
         }
     }
 }
